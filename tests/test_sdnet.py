73a618b625da13fbcd709739917cab09203afcd2
Philipp <phiyodr@gmail.com>
2022-01-21 21:49:14 +0100

    Add tests for sdnet_binary

--- tests/test_sdnet.py
+++ tests/test_sdnet.py
@@ -27,48 +27,53 @@ if home_path in travis_homes:
     img_pil = Image.fromarray(np.uint8(img_np)).convert('RGB')
     img_pil.save(image_file)
 
-
-def test_sdnet_basic():
-    for name in ["sdnet", "sdnet_binary"]:
-        all_dataset = BikitDataset(name="sdnet", split="")
-        transform_dataset = BikitDataset(name="sdnet", split="",
-            transform=transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()]))
-        train_dataset = BikitDataset(name="sdnet", split="train")
-        val_dataset = BikitDataset(name="sdnet", split="val")
-        test_dataset = BikitDataset(name="sdnet", split="test")
-        development_dataset = BikitDataset(name="sdnet", split="test", devel_mode=True)
-        img, targets = all_dataset[0]
-        assert img.dtype == torch.float32
-        assert targets.dtype == torch.float32
-        assert list(img.shape) == [3, 256, 256]
+@pytest.mark.parametrize("name", ["sdnet", "sdnet_binary"])
+def test_sdnet_basic(name):
+    all_dataset = BikitDataset(name, split="")
+    transform_dataset = BikitDataset(name, split="",
+                                     transform=transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()]))
+    train_dataset = BikitDataset(name, split="train")
+    val_dataset = BikitDataset(name, split="val")
+    test_dataset = BikitDataset(name, split="test")
+    development_dataset = BikitDataset(name, split="test", devel_mode=True)
+    img, targets = all_dataset[0]
+    assert img.dtype == torch.float32
+    assert targets.dtype == torch.float32
+    assert list(img.shape) == [3, 256, 256]
+    if name == "sdnet":
         assert list(targets.shape) == [6]
+    elif name == "sdnet_binary":
+        assert list(targets.shape) == [2]
 
-        # Dataset length
-        assert len(all_dataset) == 56092
-        assert len(train_dataset) == 50488
-        assert len(val_dataset) == 2808
-        assert len(test_dataset) == 2796
-        assert len(development_dataset) == 100
-        assert len(transform_dataset) == 56092
-
+    # Dataset length
+    assert len(all_dataset) == 56092
+    assert len(train_dataset) == 50488
+    assert len(val_dataset) == 2808
+    assert len(test_dataset) == 2796
+    assert len(development_dataset) == 100
+    assert len(transform_dataset) == 56092
 
 
 @pytest.mark.skipif(home_path in travis_homes,
                     reason="Long-running test with real datasets for local use only, not on Travis.")
-def test_sdnet_local():
+@pytest.mark.parametrize("name", ["sdnet", "sdnet_binary"])
+def test_sdnet_local(name):
 
     # This test requieres at least 15GB of free RAM to work!
     #all_in_mem_dataset = BikitDataset(name="sdnet", split="", load_all_in_mem=True)
-    all_in_mem_develmode = BikitDataset(name="sdnet", split="", load_all_in_mem=True, devel_mode=True)
+    all_in_mem_develmode = BikitDataset(name, split="", load_all_in_mem=True, devel_mode=True)
 
 
     #assert len(all_in_mem_dataset) == 56092
     assert len(all_in_mem_develmode) == 100
 
     #Test correct cache_dir func
-    cache_test = BikitDataset(name="sdnet", split="", cache_dir=Path(os.path.join(os.path.expanduser("~"), ".bikit")))
+    cache_test = BikitDataset(name, split="", cache_dir=Path(os.path.join(os.path.expanduser("~"), ".bikit")))
     img, targets = cache_test[0]
-    assert list(targets.shape) == [6]
+    if name == "sdnet":
+        assert list(targets.shape) == [6]
+    elif name == "sdnet_binary":
+        assert list(targets.shape) == [2]
 
 if __name__ == '__main__':
     test_sdnet_local()
